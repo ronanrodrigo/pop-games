5b88e439f0f7a86a3a8d98a0f0b59d3bb0feba5c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-15 14:30:10 -0300

    Create identifier constants file

---
+++ PopGames/Constants/Identifier.swift
@@ -0,0 +1,3 @@
+struct Identifier {
+    static let gamesCollection = "GameCollectionViewCellIdentifier"
+}
--- PopGames/Games/DataSources/GamesCollectionViewDataSource.swift
+++ PopGames/Games/DataSources/GamesCollectionViewDataSource.swift
@@ -2,15 +2,13 @@ import UIKit
 
 class GamesCollectionViewDataSource: NSObject, UICollectionViewDataSource {
 
-    private let cellIdentifier = "GameCollectionViewCell"
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
 
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifier.gamesCollection, for: indexPath)
         cell.backgroundColor = UIColor.red
         return cell
     }
