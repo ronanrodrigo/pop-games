18e7436c8fb37a132c7ff90a54a3a5fddd01e003
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-17 09:59:18 -0300

    Update game entity

--- PopGames/Games/Entities/GameEntity.swift
+++ PopGames/Games/Entities/GameEntity.swift
@@ -1,11 +1,11 @@
 protocol Game {
     var name: String { get }
-    var viewers: Int { get }
     var popularity: Int { get }
+    var viewers: Int { get }
 }
 
-struct GameEntity: Game, Decodable {
+struct GameEntity: Game {
     let name: String
-    let viewers: Int
     let popularity: Int
+    let viewers: Int
 }
