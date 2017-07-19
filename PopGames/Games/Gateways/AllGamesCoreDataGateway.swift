21e9c45c18a1221dcf8b59cb0338e74e03797c22
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-19 20:59:26 -0300

    Create AllGamesCoreDataGateway file

---
+++ PopGames/Games/Gateways/AllGamesCoreDataGateway.swift
@@ -0,0 +1,12 @@
+import CoreData
+
+struct AllGamesCoreDataGateway: AllGamesGateway {
+
+//    private let entityName = String(describing: TaskEntityCoreData.self)
+//    private let persistentContainer: NSPersistentContainer
+
+    func allGames(onComplete: ((Result<[Game]>) -> Void)) {
+    }
+
+}
+
