d86d7f13d70e233a4e05f1e1a64263ab7b4c8e33
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-19 20:41:16 -0300

    Create list top games usecase

---
+++ PopGames/Games/Usecases/ListTopGamesUseCase.swift
@@ -0,0 +1,20 @@
+struct ListTopGamesUseCase {
+
+    private let presenter: ListTopGamesPresenter
+    private let gateway: AllGamesGateway
+
+    init(presenter: ListTopGamesPresenter, allGamesGateway: AllGamesGateway) {
+        self.presenter = presenter
+        self.gateway = allGamesGateway
+    }
+
+    func list() {
+        gateway.allGames { result in
+            switch result {
+            case .success(let games): self.presenter.list(games: games)
+            case .fail(let error): self.presenter.show(error: error)
+            }
+        }
+    }
+
+}
---
+++ PopGames/Helpers/PopGamesError.swift
@@ -0,0 +1,3 @@
+enum PopGamesError: Error {
+    case invalidRequest
+}
