954cfa8af30d4bd112adf5f694a0f573d638c876
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-19 20:52:24 -0300

    Create core data stack abstraction

--- PopGames/Essentials/AppDelegate.swift
+++ PopGames/Essentials/AppDelegate.swift
@@ -4,30 +4,15 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    private lazy var coreDataStack: CoreDataStack = CoreDataStack()
     var window: UIWindow?
 
-    // MARK: - Core Data stack
-
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "PopGames")
-        container.loadPersistentStores(completionHandler: { (_, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
+        return self.coreDataStack.persistentContainer
     }()
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+    func applicationWillTerminate(_ application: UIApplication) {
+        coreDataStack.saveContext()
     }
 
 }
---
+++ PopGames/Helpers/CoreDataStack.swift
@@ -0,0 +1,48 @@
+import Foundation
+import CoreData
+
+public class CoreDataStack {
+
+    public init() {}
+
+    private lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "PopGames")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    public lazy var persistentContainer: NSPersistentContainer = {
+        self.container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return self.container
+    }()
+
+    public func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
