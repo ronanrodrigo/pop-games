fe5ced6cefb22180a430e65d4bbe260f542281e8
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-19 20:53:21 -0300

    Update CoreDataStack to solve warnigns

--- PopGames/Helpers/CoreDataStack.swift
+++ PopGames/Helpers/CoreDataStack.swift
@@ -16,16 +16,8 @@ public class CoreDataStack {
     }()
 
     public lazy var persistentContainer: NSPersistentContainer = {
-        self.container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        self.container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -45,4 +37,3 @@ public class CoreDataStack {
     }
 
 }
-
