519b8b71ff9ea09a4f629f346a03ce325b8a4a91
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-19 22:52:21 -0300

    Update AllGamesCoreDataGateway and add fetch request

--- PopGames/Games/Gateways/AllGamesCoreDataGateway.swift
+++ PopGames/Games/Gateways/AllGamesCoreDataGateway.swift
@@ -2,10 +2,27 @@ import CoreData
 
 struct AllGamesCoreDataGateway: AllGamesGateway {
 
-//    private let entityName = String(describing: TaskEntityCoreData.self)
-//    private let persistentContainer: NSPersistentContainer
+    private let persistentContainer: NSPersistentContainer
 
-    func allGames(onComplete: ((Result<[Game]>) -> Void)) {
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
     }
 
+    func allGames(onComplete: ((Result<[Game]>) -> Void)) {
+        let fetchGames: NSFetchRequest<GameEntityCoreData> = GameEntityCoreData.fetchRequest()
+
+        do {
+            let games = try persistentContainer.viewContext.fetch(fetchGames)
+            let gamesEntities = games.map(generateEntity)
+            onComplete(Result.success(gamesEntities))
+        } catch {
+            onComplete(Result.fail(error))
+        }
+    }
+
+    private func generateEntity(game: GameEntityCoreData) -> Game {
+        return GameEntity(name: game.name ?? String.empty,
+                          popularity: game.popularity.int,
+                          viewers: game.viewers.int)
+    }
 }
