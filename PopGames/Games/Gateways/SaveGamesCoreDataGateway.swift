7e9d393577cbd60f07f5435eaad98aaa95fa9818
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-22 01:19:55 -0300

    Update SaveGamesCoreDataGateway

--- PopGames/Games/Gateways/SaveGamesCoreDataGateway.swift
+++ PopGames/Games/Gateways/SaveGamesCoreDataGateway.swift
@@ -9,24 +9,27 @@ struct SaveGamesCoreDataGateway: SaveGamesGateway {
     }
 
     func save(games: [Game]) -> Future<Void> {
-
         return Future { completion in
             let entityName = String(describing: GameEntityCoreData.self)
             for game in games {
-                let gameCoreData = NSEntityDescription.insertNewObject(forEntityName: entityName,
-                                                                       into: managedObjectContext) as? GameEntityCoreData
+                let gameCoreData = NSEntityDescription.insertNewObject(
+                    forEntityName: entityName,
+                    into: managedObjectContext) as? GameEntityCoreData
                 gameCoreData?.name = game.name
                 gameCoreData?.popularity = Int32(game.popularity)
                 gameCoreData?.viewers = Int32(game.viewers)
             }
-            do {
-                try managedObjectContext.save()
-                completion(Result.success(()))
-            } catch {
-                completion(Result.fail(error))
-            }
+            completion(generateResult())
+        }
+    }
+
+    private func generateResult() -> Result<Void> {
+        do {
+            try managedObjectContext.save()
+            return Result.success(())
+        } catch {
+            return Result.fail(error)
         }
     }
 
 }
-
