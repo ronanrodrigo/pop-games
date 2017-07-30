df389fdc33afd6fd85a34e5e5e28d4f4c800c2cb
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-30 19:33:19 -0300

    Add `id`, `cover` and `coverUrl` to core data entity

--- PopGames/Games/Entities/GameEntityCoreData+CoreDataProperties.swift
+++ PopGames/Games/Entities/GameEntityCoreData+CoreDataProperties.swift
@@ -4,9 +4,12 @@ import CoreData
 extension GameEntityCoreData {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<GameEntityCoreData> {
-        return NSFetchRequest<GameEntityCoreData>(entityName: String(describing: GameEntityCoreData.self))
+        return NSFetchRequest<GameEntityCoreData>(entityName: "GameEntityCoreData")
     }
 
+    @NSManaged public var cover: NSData?
+    @NSManaged public var coverUrl: String?
+    @NSManaged public var id: Int32
     @NSManaged public var name: String?
     @NSManaged public var popularity: Int32
     @NSManaged public var viewers: Int32
