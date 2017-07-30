7bbf6ede3b8e670d5d07551952f67457b82ede57
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-30 19:48:44 -0300

    Add `id`, `cover` and `coverUrl` to decodable entities

--- PopGames/Games/Entities/TwitchTopGamesDecodable.swift
+++ PopGames/Games/Entities/TwitchTopGamesDecodable.swift
@@ -1,13 +1,28 @@
 struct TwitchTopGamesDecodable: Decodable {
+
     let top: [TopGameDecodable]
+
+}
+
+extension TwitchTopGamesDecodable {
 
     struct TopGameDecodable: Decodable {
         let game: GameDecodable
         let viewers: Int
+    }
 
-        struct GameDecodable: Decodable {
-            let name: String
-            let popularity: Int
+}
+
+extension TwitchTopGamesDecodable.TopGameDecodable {
+
+    struct GameDecodable: Decodable {
+        let name: String
+        let popularity: Int
+        let id: Int
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id", popularity, name
         }
     }
+
 }
