6211c651f0546432ee5bb851d2f0ff509ebcca8b
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-07-31 14:56:29 -0300

    Remove queue management

--- PopGames/Games/Gateways/SaveGamesCoreDataGateway.swift
+++ PopGames/Games/Gateways/SaveGamesCoreDataGateway.swift
@@ -13,20 +13,16 @@ struct SaveGamesCoreDataGateway: SaveGamesGateway {
             let entityName = String(describing: GameEntityCoreData.self)
             for game in games {
                 var gameCoreData: GameEntityCoreData?
-                DispatchQueue.main.async {
-                    gameCoreData = NSEntityDescription.insertNewObject(
-                        forEntityName: entityName,
-                        into: self.managedObjectContext) as? GameEntityCoreData
-                    gameCoreData?.id = Int32(game.id)
-                    gameCoreData?.coverUrl = game.coverUrl
-                    gameCoreData?.name = game.name
-                    gameCoreData?.popularity = Int32(game.popularity)
-                    gameCoreData?.viewers = Int32(game.viewers)
-                }
+                gameCoreData = NSEntityDescription.insertNewObject(
+                    forEntityName: entityName,
+                    into: self.managedObjectContext) as? GameEntityCoreData
+                gameCoreData?.id = Int32(game.id)
+                gameCoreData?.coverUrl = game.coverUrl
+                gameCoreData?.name = game.name
+                gameCoreData?.popularity = Int32(game.popularity)
+                gameCoreData?.viewers = Int32(game.viewers)
             }
-            DispatchQueue.main.async {
-                completion(self.generateResult())
-            }
+            completion(self.generateResult())
         }
     }
 
